9b3b5666d2183f8433e6878cbd3745bf043e47cb
tung1100i <1851063190@e.tlu.edu.vn>
2022-09-09 18:29:54 +0700

    oke

---
+++ src/main/java/com/example/JavaTokenWeb_ver1/AuthenBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.JavaTokenWeb_ver1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenBasicApplication.class, args);
+    }
+
+}
--- src/main/java/com/example/JavaTokenWeb_ver1/configweb/Config.java
+++ src/main/java/com/example/JavaTokenWeb_ver1/configweb/Config.java
@@ -21,57 +21,24 @@ public class Config extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().ignoringAntMatchers("/api/login")
-                .and()
-                .cors() // Ngăn chặn request từ một domain khác
-                .and()
+
                 .authorizeRequests()
-                .antMatchers("/", "/home", "/api/login").permitAll()
+                .antMatchers("/", "/home").permitAll()
                 .antMatchers("/accounts/**/**").hasAuthority("ADMIN")
                 .anyRequest().authenticated()
                 .and()
-                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-//        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+                .formLogin().permitAll()
+                .and()
+                .logout().permitAll()
+                .and()
+                .csrf().disable()
+                .httpBasic();
     }
-
-
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .cors() // Ngăn chặn request từ một domain khác
-//                .and()
-//                .authorizeRequests()
-//                .antMatchers( "/api/login").permitAll()
-//                .antMatchers("/accounts/**/**").hasAuthority("ADMIN")// Cho phép tất cả mọi người truy cập vào địa chỉ này
-//                .anyRequest().authenticated()
-////                .and()
-////                .formLogin().permitAll()
-////                .and()
-////                .logout().permitAll()
-//                 ;// Tất cả các request khác đều cần phải xác thực mới được truy cập
-//
-//
-//
-//        // Thêm một lớp Filter kiểm tra jwt
-//        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-//    }
 
     // khai báo Bean
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public JwtAuthenticationFilter jwtAuthenticationFilter() {
-        return new JwtAuthenticationFilter();
-    }
-
-    @Bean(BeanIds.AUTHENTICATION_MANAGER)
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        // Get AuthenticationManager bean
-        return super.authenticationManagerBean();
     }
 
     @Autowired
